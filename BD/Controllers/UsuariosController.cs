8c462dee7a3fd28a3309f76e087d18611e850611
0220832111009 <0220832111009@LAB4TC28>
2022-03-16 10:59:26 -0300

    Adicionar arquivos de projeto.

---
+++ BD/Controllers/UsuariosController.cs
@@ -0,0 +1,69 @@
+﻿using BD.entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BD.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public readonly Contexto db;
+        public UsuariosController(Contexto banco)
+        {
+            db = banco;
+        }
+
+
+
+        // GET: UsuariosController
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList() );
+        }
+
+        // GET: UsuariosController/Details/5
+
+        // GET: UsuariosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario collection)
+        {
+            db.Usuarios.Add(collection);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        // GET: UsuariosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View( db.Usuarios.Where(a => a.Id == id).FirstOrDefault());
+        }
+
+        // POST: UsuariosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Usuario collection)
+        {
+            db.Usuarios.Update(collection);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        // GET: UsuariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Delete/5
+    }
+}
---
+++ BD/Migrations/20220316120058_Inicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BD.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
